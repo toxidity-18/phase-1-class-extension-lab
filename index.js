c5bfa6e5a1885e4926c17f4f88fe0ec74c7f0900
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-21 12:24:57 +0300

    first

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Your code here
+
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+  
+  
+  class Triangle extends Polygon {
+    constructor(sides) {
+      if (sides.length !== 3) {
+        throw new Error('A triangle must have 3 sides');
+      }
+      super(sides);
+    }
+  
+    get isValid() {
+      const [a, b, c] = this.sides;
+      return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+  }
+  
+
+  class Square extends Polygon {
+    constructor(sides) {
+      if (sides.length !== 4) {
+        throw new Error('A square must have 4 sides');
+      }
+      super(sides);
+    }
+  
+    get isValid() {
+      return this.sides.every(side => side === this.sides[0]);
+    }
+  
+    get area() {
+      if (!this.isValid) {
+        throw new Error('Not a valid square');
+      }
+      return this.sides[0] ** 2;
+    }
+  }
+  
+ 
+  const triangle = new Triangle([3, 4, 5]);
+  console.log(triangle.countSides);  
+  console.log(triangle.perimeter);   
+  console.log(triangle.isValid);     
+  
+  const square = new Square([4, 4, 4, 4]);
+  console.log(square.countSides);  
+  console.log(square.perimeter);     
+  console.log(square.isValid);       
+  console.log(square.area);        
+  
